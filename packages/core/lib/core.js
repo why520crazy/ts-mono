b9e5adcc56169e4017225fb570f9465564b63c6f
why520crazy <why520crazy@163.com>
2020-02-04 20:25:03 +0800

    fix: remove comment

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -1,11 +1,5 @@
 "use strict";
 
-// module.exports = core;
-
-// function core() {
-//     console.log(`this is core`);
-// }
-
 exports.hello = function() {
     console.log(`this is hello from core`);
 };
